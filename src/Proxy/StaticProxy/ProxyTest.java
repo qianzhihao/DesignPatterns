49a309c4f9b81c2a6ea015205923e1109d8f1f36
qianzhihao <tonyqianzhihao@qq.com>
2019-01-30 09:07:12 +0800

    StaticProxy Pattern

---
+++ src/Proxy/StaticProxy/ProxyTest.java
@@ -0,0 +1,15 @@
+package Proxy.StaticProxy;
+
+/**
+ * @author qianzhihao
+ * @Description 测试类
+ * @Date 2019/1/10
+ */
+public class ProxyTest {
+    public static void main(String[] args) {
+        BuyHouse buyHouse = new BuyHouseImpl();
+        buyHouse.buyHouse();
+        BuyHouseProxy buyHouseProxy = new BuyHouseProxy(buyHouse);
+        buyHouseProxy.buyHouse();
+    }
+}
