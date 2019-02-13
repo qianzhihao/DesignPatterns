e777e4cd8ea309336e0f2d7c9e37bc1081ee0cc2
qianzhihao <tonyqianzhihao@qq.com>
2019-02-13 16:26:03 +0800

    LKP principle：最小知识原则2 软件安装程序

---
+++ src/principles/leastKnowledge/Client1.java
@@ -0,0 +1,8 @@
+package principles.leastKnowledge;
+
+public class Client1 {
+    public static void main(String[] args) {
+        InstallSoftware invoker = new InstallSoftware();
+        invoker.installWizard(new Wizard());
+    }
+}
---
+++ src/principles/leastKnowledge/InstallSoftware.java
@@ -0,0 +1,8 @@
+package principles.leastKnowledge;
+
+public class InstallSoftware {
+    public void installWizard(Wizard wizard) {
+        //直接调用
+        wizard.installWizard();
+    }
+}
