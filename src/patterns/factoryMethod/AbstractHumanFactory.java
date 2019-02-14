2ca1892de636816f2038ef45725fdb4fbf0227b9
qianzhihao <tonyqianzhihao@qq.com>
2019-02-14 17:23:33 +0800

    工厂模式： 实例：女娲造人

---
+++ src/patterns/factoryMethod/AbstractHumanFactory.java
@@ -0,0 +1,5 @@
+package patterns.factoryMethod;
+
+public abstract class AbstractHumanFactory {
+    public abstract <T extends Human> T createHuman(Class<T> c);
+}
---
+++ src/patterns/factoryMethod/WhiteHuman.java
@@ -0,0 +1,11 @@
+package patterns.factoryMethod;
+
+public class WhiteHuman implements Human {
+    public void getColor() {
+        System.out.println("白色人种的皮肤颜色是白色的！");
+    }
+
+    public void talk() {
+        System.out.println("白色人种会说话，一般人说的是单字节。");
+    }
+}
