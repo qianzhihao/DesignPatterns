8177bf75810ba11ab847f99e18db9cd13ce7f993
qianzhihao <tonyqianzhihao@qq.com>
2019-02-14 13:07:07 +0800

    单例模式扩展：有上限的多例模式

--- src/patterns/singleton/Emperor.java
+++ src/patterns/singleton/Emperor.java
@@ -1,15 +1,38 @@
 package patterns.singleton;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Emperor {
-    //初始化一个皇帝
-    private static final Emperor emperor = new Emperor();
+
+    private static int maxNumOfEmperor = 2;
+
+    private static ArrayList<String> nameList = new ArrayList<String>();
+
+    private static ArrayList<Emperor> emperorList = new ArrayList<Emperor>();
+
+    private static int countNumOfEmperor = 0;
+
+    static {
+        for (int i = 0; i < maxNumOfEmperor; i++) {
+            emperorList.add(new Emperor("皇" + (i+1) + "帝"));
+        }
+    }
+
+//    //初始化一个皇帝
+//    private static final Emperor emperor = new Emperor();
     private Emperor() {
         //世俗和道德约束，目的是不希望产生第二个皇帝
     }
+    private Emperor(String name) {
+        nameList.add(name);
+    }
     public static Emperor getInstance() {
-        return emperor;
+        Random random = new Random();
+        countNumOfEmperor = random.nextInt(maxNumOfEmperor);
+        return emperorList.get(countNumOfEmperor);
     }
     public static void say() {
-        System.out.println("我就是皇帝赵匡胤");
+        System.out.println(nameList.get(countNumOfEmperor));
     }
 }
--- src/patterns/singleton/Minister.java
+++ src/patterns/singleton/Minister.java
@@ -2,8 +2,12 @@ package patterns.singleton;
 
 public class Minister {
     public static void main(String[] args) {
-        for (int day=0;day<3;day++) {
+
+        int ministerNum = 5;
+
+        for (int i=0;i<ministerNum;i++) {
             Emperor emperor = Emperor.getInstance();
+            System.out.println("第"+(i+1)+"个大臣参拜的是：");
             emperor.say();
         }
     }
