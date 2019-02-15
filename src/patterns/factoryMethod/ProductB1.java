5ba81171040b5ff272f90d7f2cee640561b4ab3a
qianzhihao <tonyqianzhihao@qq.com>
2019-02-15 11:32:43 +0800

    抽象工厂模式的定义

---
+++ src/patterns/factoryMethod/ProductB1.java
@@ -0,0 +1,8 @@
+package patterns.factoryMethod;
+
+public class ProductB1 extends AbstractProductB {
+    @Override
+    public void doSomething() {
+        System.out.println("产品B1的实现方法");
+    }
+}
