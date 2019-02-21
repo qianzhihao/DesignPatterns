4a2153497f20f48aeecb287a38550b89b5480d9c
qianzhihao <qianzhihao@jsoa.net>
2019-02-21 11:33:34 +0800

    123

--- src/utils/excelUtils/Client.java
+++ src/utils/excelUtils/Client.java
@@ -8,7 +8,8 @@ public class Client {
 
         //TODO
         //1. 上传 xls 文件到指定目录；
-        //2.读取上一步上传的 xls 文件
+        //2.读取上一步上传的 xls 文件并解析成 List<Object>;
+        //3.持久化 List<Object> 中的每个 Object；
 
         List<ApiInfo> apiInfoList =  Excel2Object.loadApiInfo("E:\\testworkbook.xls");
         System.out.println("表格中一共有"+ apiInfoList.size() +"个api");
