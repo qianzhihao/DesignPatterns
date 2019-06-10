019e591a30c947e94912d39c005854de2c0779d8
qianzhihao <qianzhihao@jsoa.net>
2019-06-10 17:25:04 +0800

    组合模式 -- 定义

---
+++ src/patterns/composite/define/Component.java
@@ -0,0 +1,7 @@
+package patterns.composite.define;
+
+public abstract class Component {
+    public void doSomething() {
+        System.out.println("component business");
+    }
+}
---
+++ src/patterns/composite/define/Leaf.java
@@ -0,0 +1,5 @@
+package patterns.composite.define;
+
+public class Leaf extends Component {
+    //可以覆写父类方法
+}
