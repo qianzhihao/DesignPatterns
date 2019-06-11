1494c4a391690bd4808b0f12e4ea2fed3a03cae4
qianzhihao <qianzhihao@jsoa.net>
2019-06-11 14:43:14 +0800

    备忘录模式 -- 定义

---
+++ src/patterns/memento/define/Memento.java
@@ -0,0 +1,14 @@
+package patterns.memento.define;
+
+public class Memento {
+    private String state = "";
+    public Memento(String _state) {
+        this.state = _state;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+}
