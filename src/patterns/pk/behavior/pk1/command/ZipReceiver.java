79aa4efb18e0d06e077c3f7375448e74c38b438e
qianzhihao <qianzhihao@jsoa.net>
2019-06-21 14:18:51 +0800

    Revert "行为类模式PK - 命令模式vs策略模式"
    This reverts commit 5ed4bc5

---
+++ src/patterns/pk/behavior/pk1/command/ZipReceiver.java
@@ -0,0 +1,15 @@
+package patterns.pk.behavior.pk1.command;
+
+public class ZipReceiver implements IReceiver {
+    @Override
+    public boolean compress(String source, String to) {
+        System.out.println(source + "-->" + to + "Zip Compress Success!");
+        return true;
+    }
+
+    @Override
+    public boolean uncompress(String source, String to) {
+        System.out.println(source + "-->" + to + "\n Zip UnCompress Success!");
+        return true;
+    }
+}
